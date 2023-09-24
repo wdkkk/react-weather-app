74b9e5343fb9a5d694a93283001d1c8644830f2b
wdk <61912053+wdkkk@users.noreply.github.com>
2023-09-24 16:56:40 +0800

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,5 @@
 const constants = {
-  BASE_URL: "http://api.openweathermap.org/",
+  BASE_URL: "https://api.openweathermap.org/",
   API_KEY: "12a3a0ea08f23536a015362f85402a6e",
 };
 //http://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=12a3a0ea08f23536a015362f85402a6e
