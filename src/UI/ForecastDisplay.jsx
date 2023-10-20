c38ddf19f18854e0c1e711eca7a801c32fa627aa
wdk <61912053+wdkkk@users.noreply.github.com>
2023-10-21 02:42:55 +0800

    Update ForecastDisplay.jsx

--- src/UI/ForecastDisplay.jsx
+++ src/UI/ForecastDisplay.jsx
@@ -1,24 +1,16 @@
 import React from "react";
 
 import "./style.sass";
-import weather from "../constants/weather";
+import constants from "../constants/constants";
 
 const ForecastDisplay = ({ forecast }) => {
   const convertTemp = (temp) => Math.round(temp - 273.15);
-
-  const weatherDesc = weather.find((o) => o.name === forecast.weather[0].main)
-    ? weather.find((o) => o.name === forecast.weather[0].main)
-    : "loading";
 
   return (
     <div className="ForecastDisplay">
       <div className="LocationDisplayCity">{forecast.dt_txt.slice(10, 16)}</div>
       <div className="LocationDisplayImage">
-        {weatherDesc === "loading" ? (
-          <div className="loading"></div>
-        ) : (
-          <img src={weatherDesc.IMG_URL} alt="" />
-        )}
+        <img src={`${constants.IMG_URL}${forecast.weather[0].icon}.png`} alt="" />
       </div>
       <div className="LocationDisplayTemp">
         {convertTemp(forecast.main.temp)}°C
